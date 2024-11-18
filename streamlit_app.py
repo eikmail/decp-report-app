9273e34717e134efbf7a450874f6cbeb0d82323e
Your Name <you@example.com>
2024-11-18 18:51:32 +0100

    Added generic request call for all sources

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,7 +19,7 @@ def check_password():
     def password_entered():
         """Checks whether a password entered by the user is correct."""
         conn = st.connection("decp_database")
-        data = conn.query("select account_id,source_id from decp_report.account where user_name=:user_name and passwd = :passwd", ttl="10s",params={"user_name":st.session_state["username"],"passwd":st.session_state["password"]})
+        data = conn.query("select account_id,source_id from decp_report.account where user_name=:user_name and passwd = :passwd",ttl="10s",params={"user_name":st.session_state["username"],"passwd":st.session_state["password"]})
         if 'account_id' in data and len(data['account_id'])>0 and data['account_id'][0]>0:
             st.session_state["password_correct"] = True
             st.session_state["source_id"] = data['source_id'][0]
@@ -45,10 +45,10 @@ if not check_password():
 # Main Streamlit app starts here
 
 if "source_id" in st.session_state and st.session_state.get("source_id", None) is not None:
-    source_id = st.session_state.get("source_id", None)
+    source_id = int(st.session_state.get("source_id", None))
 
     conn = st.connection("decp_database")
-    df = conn.query("select name from decp_report.source WHERE source_id = :source_id",params={"source_id":source_id})
+    df = conn.query("select name from decp_report.source WHERE source_id = :source_id",ttl="10s",params={"source_id":source_id})
     source_name = df['name'][0]
 else:
     # Get sources list
@@ -62,16 +62,21 @@ else:
 if source_id is None:
     st.write("Affichage de toutes les sources")
 
+    # Request for all source query
     conn = st.connection("decp_database")
-    data = conn.query("select session_date,nb_records_dematis,per_errors_dematis,nb_records_pes,per_errors_pes from decp_report.v_stats_global", ttl="10s")
+    data = conn.query("SELECT decp_report.get_query_stats_global()", ttl="10s")
 
-    st.line_chart(data,x='session_date')
-    st.dataframe(data)
+    if 'get_query_stats_global' in data and len(data['get_query_stats_global'])>0:
+        query = data['get_query_stats_global'][0]
+        
+        data = conn.query(query, ttl="10s")
+        
+        st.line_chart(data,x='session_date')
+        st.dataframe(data)
 else:
     st.header(source_name)
     
     conn = st.connection("decp_database")
-    data = conn.query("SELECT session_date,nb_records,nb_errors,per_errors from decp_report.v_stats_all WHERE source_id = :source_id", ttl="10s", params={"source_id": source_id})
-    
+    data = conn.query("SELECT session_date,nb_records,nb_errors,per_errors from decp_report.v_stats_all WHERE source_id = :source_id",ttl="10s",params={"source_id":source_id})    
     st.line_chart(data,x='session_date')
     st.dataframe(data)
