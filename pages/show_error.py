bda00367af3cd709c58474aac5314793a27011aa
Your Name <you@example.com>
2024-12-19 11:44:24 +0100

    Enhanced error list

--- pages/show_error.py
+++ pages/show_error.py
@@ -13,6 +13,23 @@ if not streamlit_app.check_password():
 # For session_id conversion
 register_adapter(np.int64, AsIs)
 
+query_errors = """
+    SELECT 
+        (SELECT name FROM decp_report.step s WHERE s.step_id = r.step_id) AS step,
+        (SELECT name FROM decp_report."source" s WHERE s.source_id = r.source_id) AS source,
+        (SELECT name FROM decp_report.file f WHERE f.file_id = r.file_id) AS file,
+        (SELECT name FROM decp_report.exclusion_type et WHERE et.exclusion_type_id = r.exclusion_type_id) AS type,
+        r."position", 
+        r.exclusion_type_id,
+        r.message,
+        r.error,
+        r."path",
+        r.id_content 
+    FROM decp_report.report r 
+    WHERE r.exclusion_type_id = 1
+    AND session_id = :session_id
+"""
+
 if "source_id" in st.session_state and st.session_state.get("source_id", None) is not None:
     source_id = int(st.session_state.get("source_id", None))
 
@@ -42,8 +59,9 @@ if source_id is None:
     session_id = df.loc[df["full_name"] == session_name, 'session_id'].iloc[0]
 
     if session_id is not None:
-        data = conn.query("SELECT * FROM decp_report.report WHERE session_id = :session_id",ttl="10s",params={"session_id": session_id})    
+        data = conn.query(query_errors,ttl="10s",params={"session_id": session_id})    
         st.dataframe(data)
+
 else:
     st.header(source_name)
 
@@ -59,7 +77,7 @@ else:
         data = conn.query("SELECT session_date,nb_records from decp_report.v_stats_all WHERE source_id = :source_id and session_id = :session_id",ttl="10s",params={"source_id":source_id,"session_id": session_id})    
         st.line_chart(data,x='session_date')
 
-        data = conn.query("SELECT * FROM decp_report.report WHERE source_id = :source_id and session_id = :session_id",ttl="10s",params={"source_id":source_id,"session_id": session_id})    
+        query_errors += " source_id = :source_id"
+        data = conn.query(query_errors,ttl="10s",params={"source_id":source_id,"session_id": session_id})    
         st.dataframe(data)
 
-st.write("End")
